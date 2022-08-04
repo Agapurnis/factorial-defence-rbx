4d07ee472aefc1d319f39ff6f923978800e70243
Katini <agapurnis@outlook.com>
2022-08-03 21:19:07 -0700

    chore: remove old debug code
    I swear I did this earlier? Not talking about the server one, either.
    I thought I deleted this, then couldn't find the server one.
    Then I just deleted the server, and now I didn't delete this one? Wack.

--- src/StarterPlayer/StarterPlayerScripts/test.client.ts
+++
@@ -1,9 +0,0 @@
-import { Result } from "@rbxts/rust-classes";
-import Remotes from "ReplicatedStorage/Networking";
-
-while(task.wait(3) !== undefined) {
-	// spawn(() => {
-	// 	Remotes.Test.A.Invoke(Result.ok(true));
-	// });
-}
-
